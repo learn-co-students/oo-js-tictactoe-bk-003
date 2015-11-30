69a9171d90b5ba0071c85ce0b8508d8aa3734b0e
bmesa620 <bmesa620@gmail.com>
2015-11-30 00:09:50 -0500

    tough to say the least

--- js/game.js
+++ js/game.js
@@ -1,33 +1,77 @@
 function Game() {}
 
 Game.prototype.init = function (board) {
-  // code here
+  this.board = board;
+  this.turn = 0;
+  this.createState();
 }
 
 Game.prototype.checkCells = function(ary) {
-  // code here
+  var winner = ""
+  var winningArrays = [
+    [this.state[0], this.state[1], this.state[2]],
+    [this.state[3], this.state[4], this.state[5]],
+    [this.state[6], this.state[7], this.state[8]],
+    [this.state[0], this.state[3], this.state[6]],
+    [this.state[1], this.state[4], this.state[7]],
+    [this.state[2], this.state[5], this.state[8]],
+    [this.state[0], this.state[4], this.state[8]],
+    [this.state[2], this.state[4], this.state[6]]
+    ];
+    winningArrays.forEach(function(winCombo) {
+      if (winCombo[0] == winCombo[1] && winCombo[1] == winCombo[2] && winCombo[0] == this.player()) {
+        winner = winCombo[0];
+      }
+    },this);
+    if (winner == this.player()) {
+      return winner;
+    }
 }
 
 Game.prototype.checkWinner = function() {
-  // code here
+ if (this.checkCells(this.state) == this.player()) {
+    this.message('Player ' + this.checkCells(this.state) + ' Won!');
+  }
+  else {
+    return false;
+  }
 }
 
 Game.prototype.player = function() {
-  // code here
+  if (this.turn % 2 == 0) {
+		return "X";
+	} else {
+		return "O";
+	}
 }
 
 Game.prototype.doTurn = function(id){
-  // code here
+  this.updateState(id);
+  this.checkWinner();
+  this.turn += 1;
+  if (this.turn >= 9) {
+    this.message("It's a tie!");
+  }
 }
 
 Game.prototype.message = function (message) {
-  // code here
+  $('p#message').empty().append(message);
+  this.reset();
 }
 
 Game.prototype.updateState = function(id) {
-  // code here
+  if (!this.state) {
+		this.createState();
+	}
+	this.state[id] = this.player();
+	this.board.updateCell(id, this.player());
 }
 
-Game.prototype.createState = function() {
-  // code here
+Game.prototype.createState = function() { 
+  this.state = [,,,,,,,,,];
 }
+Game.prototype.reset = function() {
+  $('td').empty();
+  this.createState();
+  this.turn = 0;
+};
