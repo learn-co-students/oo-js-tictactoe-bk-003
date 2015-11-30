4bd1a0a1d48320823e7445078ca0a47afeb16949
Andrew Persad <akpersad@gmail.com>
2015-11-30 14:41:31 -0500

    done

--- js/board.js
+++ js/board.js
@@ -2,18 +2,30 @@ function Board() {}
 
 Board.prototype.init = function() {
   // code here
+  this.buildBoard()
 }
 
 Board.prototype.buildBoard = function() {
     // code here
+    var html = this.createRowArray(3, '<tr>')
+    html.forEach(function(elem, index){
+    	html[index] = elem+this.createColumnArray(3, '<td></td>').join('')+'</tr>'
+    }, this);
+    $('body').append(html.join(''))
 }
 
 Board.prototype.createRowArray = function(size, element) {
   // code here
+  var arr = []
+  for (var i=0; i<size; i++) {
+  	arr.push(element)
+  }
+  return arr;
 }
 
 Board.prototype.createColumnArray = function(size, element) {
   // code here
+  return this.createRowArray(size, element);
 }
 
 Board.prototype.createArray = function(size, element) {
@@ -22,12 +34,21 @@ Board.prototype.createArray = function(size, element) {
 
 Board.prototype.addIds = function() {
   // code here
+  $('td').each(function(index, elem){
+  	$(elem).attr('id', index)
+  })
 }
 
 Board.prototype.updateCell = function(id, mark) {
   // code here
+  $('#'+id).html(mark)
 }
 
 Board.prototype.addEvents = function(game) {
   // code here
+  $('td').each(function(index, elem){
+  	$('#'+index).click(function(){
+
+  	})
+  })
 }
--- js/game.js
+++ js/game.js
@@ -1,33 +1,63 @@
-function Game() {}
+function Game() {
+	this.turn = 0
+	this.createState()
+}
 
 Game.prototype.init = function (board) {
   // code here
+  this.board = board
+  this.turn = 0
+  this.createState()
 }
 
 Game.prototype.checkCells = function(ary) {
   // code here
+  var count = 0
+  for(var i=0;i<3;i++){
+  	this.state[ary[i]] == this.player() ? count++ : '';
+  }
+  return count==3 ? 1 : 0
 }
 
 Game.prototype.checkWinner = function() {
   // code here
+  var won = 0;
+  [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]].forEach(function(ary){
+  	won = won + this.checkCells(ary)
+  }, this)
+  return won > 0
 }
 
 Game.prototype.player = function() {
   // code here
+  return this.turn % 2 == 0 ? 'X' : 'O';
 }
 
 Game.prototype.doTurn = function(id){
   // code here
+  this.updateState(id)
+  if (this.checkWinner()) {
+  	this.message('Player '+ this.player()+ ' Won!')
+  }
+  this.turn++
 }
 
 Game.prototype.message = function (message) {
   // code here
+  return message
 }
 
 Game.prototype.updateState = function(id) {
   // code here
+  this.state[id] = this.player()
+  this.board.updateCell(id, this.player())
 }
 
 Game.prototype.createState = function() {
   // code here
+  var arr = []
+  for(var i=0;i<9;i++) {
+  	arr.push('')
+  }
+  this.state = arr
 }
